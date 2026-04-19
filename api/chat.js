d3821ab42e20fe93f6cefcc96991da30fce77f26
kalanekalyani <167842928+kalanekalyani@users.noreply.github.com>
2026-04-19 22:07:01 +0530

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -1,3 +1,7 @@
+export const config = {
+  runtime: 'nodejs'
+};
+
 export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
